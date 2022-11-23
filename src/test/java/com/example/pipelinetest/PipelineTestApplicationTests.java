703693ef0fb484cd2acc3ceba318dd50f8acdb16
xanqus <sbk98031816@gmail.com>
2022-11-23 18:47:36 +0900

    mysql connection

---
+++ src/test/java/com/example/pipelinetest/PipelineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
