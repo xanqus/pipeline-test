cc892dce53b7e9f92aa38ca7995745f248cb4d74
xanqus <sbk98031816@gmail.com>
2022-11-28 14:41:28 +0900

    article sample

---
+++ src/main/java/com/example/pipelinetest/controller/ArticleController.java
@@ -0,0 +1,26 @@
+package com.example.pipelinetest.controller;
+
+import com.example.pipelinetest.dao.ArticleRepository;
+import com.example.pipelinetest.domain.Article;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/article")
+@RequiredArgsConstructor
+public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    @GetMapping("")
+    public List<Article> getArticles() {
+
+
+        return articleRepository.findAll();
+    }
+}
