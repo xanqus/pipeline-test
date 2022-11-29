3ed12f312dcd4dddabf6286b577b6e1973d90265
xanqus <sbk98031816@gmail.com>
2022-11-29 18:31:31 +0900

    test commit

--- src/main/java/com/example/pipelinetest/controller/ArticleController.java
+++ src/main/java/com/example/pipelinetest/controller/ArticleController.java
@@ -26,6 +26,6 @@ public class ArticleController {
 
     @GetMapping("/test")
     public String test() {
-        return "Test github webhook2";
+        return "Test github webhook~~~~~~";
     }
 }
